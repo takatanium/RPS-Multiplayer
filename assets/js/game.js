7c706f910ffea329d35888a732cbb32600bbe256
Tait Takatani <tait.takatani@gmail.com>
2017-08-27 12:50:43 -0700

    going live

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,6 +9,7 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 var intervalId;
+var whoAmI = "";
 
 $(document).ready(function() {
   game.initiate();
@@ -16,35 +17,29 @@ $(document).ready(function() {
 
 var game = {
   initiate: function() {;
-    this.findActive();
-
-    clicks.addPlayer('p1', 'Cid');
-    clicks.addPlayer('p2', 'Cloud');
-    clicks.showDown('p1');
-    clicks.showDown('p2');
+    clicks.addPlayer();
   },
-  findActive: function() {
-    let activeId = "";
+  findActive: function(userId) {
     database.ref('games/').once('value').then(function(snapshot) {
       let active = "";
       snapshot.forEach(function(child) {
-        if (child.val().active) {
+        if (!child.val().active) {
           active = child.val().id;
         }
       });
       return active;
     }).then(function(id){ 
       let init = id;
-      if (init === "") init = game.create();
+      init === "" ? init = game.create(userId) : game.join(userId, init);
       $('.arena').attr('data-gameId', init);
     });
   },
-  create: function() {
+  create: function(userId) {
     let key = database.ref('games/').push().key;
     database.ref('games/'+key).set({ 
       id: key,
-      active: true,
-      p1User: "",
+      active: false,
+      p1User: userId,
       p2User: "",
       p1Ready: false,
       p2Ready: false,
@@ -57,34 +52,53 @@ var game = {
       showdown: false,
       dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
-    console.log("create "+key);
+    $('#p1').attr('data-userId', userId);
+    $('#p1-header').html(userId);
+    game.startMonitor(key);
+    display.showdownBtn('p1');
+    whoAmI = 'p1';
     return key;
   },
-  startMonitor: function() {
-    database.ref('/games/' + $('.arena').data('gameid') + '/')
+  join: function(userId, gameId) {
+    database.ref('/games/' + gameId + '/').update({p2User: userId});
+    $('#p2').attr('data-userId', userId);
+    $('#p2-header').html(userId);
+    db.updateActive(gameId);
+    game.startMonitor(gameId);
+    display.showdownBtn('p2');
+    whoAmI = 'p2';
+  },
+  startMonitor: function(gameId) {
+    database.ref('/games/' + gameId + '/')
             .on('value', function(snapshot){
 
       sv = snapshot.val();
+
+      if (sv.p1User!=="") $('#p1-header').html(sv.p1User);
+      if (sv.p2User!=="") $('#p2-header').html(sv.p2User);
+
       if (sv.showdown && sv.timer<=0) {
+        // display.results(sv.p1User, sv.p2User, sv.p1RPS, sv.p2RPS);
+
         if (sv.p1RPS===-1 || sv.p2RPS===-1) {
           if (sv.p1RPS===-1 && sv.p2RPS===-1) {
-            game.updateHistory(-1, sv.p1Hist, sv.p2Hist);
+            db.updateHistory(-1, sv.p1Hist, sv.p2Hist, sv.p1User, sv.p2User, sv.p1RPS, sv.p2RPS);
           }
           else if (sv.p1RPS!==-1) {
-            game.updateHistory(1, sv.p1Hist, sv.p2Hist);
+            db.updateHistory(1, sv.p1Hist, sv.p2Hist, sv.p1User, sv.p2User, sv.p1RPS, sv.p2RPS);
           }
           else {
-            game.updateHistory(2, sv.p1Hist, sv.p2Hist);
+            db.updateHistory(2, sv.p1Hist, sv.p2Hist, sv.p1User, sv.p2User, sv.p1RPS, sv.p2RPS);
           }
         }
         else {
           let checkP1Win = sv.p1RPS - sv.p2RPS;
           if (checkP1Win === 0) {
-            game.updateHistory(0, sv.p1Hist, sv.p2Hist);
+            db.updateHistory(0, sv.p1Hist, sv.p2Hist, sv.p1User, sv.p2User, sv.p1RPS, sv.p2RPS);
           } else if (checkP1Win === 1 || checkP1Win === -2) {
-            game.updateHistory(1, sv.p1Hist, sv.p2Hist);
+            db.updateHistory(1, sv.p1Hist, sv.p2Hist, sv.p1User, sv.p2User, sv.p1RPS, sv.p2RPS);
           } else {
-            game.updateHistory(2, sv.p1Hist, sv.p2Hist);
+            db.updateHistory(2, sv.p1Hist, sv.p2Hist, sv.p1User, sv.p2User, sv.p1RPS, sv.p2RPS);
           }
         }
       }
@@ -93,65 +107,35 @@ var game = {
         timer.start(10);
       }
     });
-  },
-  updateHistory: function(num, p1Hist, p2Hist) {
-    switch (num) {
-      case 0: $('#result').html("<p>Tie!!</p>"); 
-              p1Hist[num]++; p2Hist[num]++; 
-              break;
-      case 1: $('#result').html("<p>Player 1 Wins!!</p>"); 
-              p1Hist[num]++; p2Hist[num+1]++; 
-              break;
-      case 2: $('#result').html("<p>Player 2 Wins!!</p>"); 
-              p1Hist[num]++; p2Hist[num-1]++; 
-              break;
-      default: $('#result').html("<p>No one is playing...</p>");
-               break;
-    }
-    let storeData = {
-      p1RPS: -1,
-      p2RPS: -1,
-      p1Hist: p1Hist,
-      p2Hist: p2Hist,
-      p1Ready: false,
-      p2Ready: false,
-      timer: -1,
-      showdown: false
-    }
-    database.ref('/games/' + $('.arena').data('gameid') + '/')
-                              .update(storeData);
   }
 }
 
 var clicks = {
-  addPlayer: function(player, userId) {
-    $('#'+player+'-add').on('click', function() {
-      $('#'+player).attr('data-user', userId);
-
-      var playerExists = false;
+  addPlayer: function() {
+    $('#join-game').on('click', function() {
+      // $('#'+player).attr('data-user', userId);
+      let userId = $('#input-user').val();
+      
       //look to see if user exists in db
       database.ref('users/').once("value").then(function(snapshot) {
+        let playerExists = false;
         snapshot.forEach(function(child) {
           cv = child.val();
-          if (cv.userId === userId) playerExists = true;
+          if (cv.userId === userId) playerExists = true; 
         });
+        return playerExists;
+      }).then(function(playerExists){
+        if (!playerExists) {
+          database.ref('users/' + userId).set({ 
+            userId: userId,
+            history: [0,0,0],
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+          });
+        }
       });
-
-      if (!playerExists) {
-        database.ref('users/' + userId).set({ 
-          userId: userId,
-          history: [0,0,0],
-          dateAdded: firebase.database.ServerValue.TIMESTAMP
-        });
-      }
-
-      //update player name
-      player === "p1" ? database.ref('/games/' + $('.arena').data('gameid') + '/')
-                              .update({p1User: userId}) :
-                        database.ref('/games/' + $('.arena').data('gameid') + '/')
-                              .update({p2User: userId});
+      game.findActive(userId);
+      display.arena();
     });
-
   },
   showDown: function(player) {
     $('#'+player+'-showdown').on('click', function() {
@@ -160,8 +144,6 @@ var clicks = {
                               .update({p1Ready: true}) :
                         database.ref('/games/' + $('.arena').data('gameid') + '/')
                               .update({p2Ready: true});
-      // let userId = $('#'+player).attr('data-user', userId);
-      game.startMonitor();
     });
   },
   choice: function(player, choice) {
@@ -195,15 +177,11 @@ var display = {
     clicks.choice(player, 'paper');
     clicks.choice(player, 'sciss');
   },
-  showdownBtn: function() {
-    let p1Btn = $('<button>').addClass('showdown-btn');
-    p1Btn.attr('id', 'p1-showdown').text('SHOWDOWN');
-    let p2Btn = $('<button>').addClass('showdown-btn');
-    p2Btn.attr('id', 'p2-showdown').text('SHOWDOWN');
-    $('#p1-btn-box').html(p1Btn);
-    $('#p2-btn-box').html(p2Btn);
-    clicks.showDown('p1');
-    clicks.showDown('p2');
+  showdownBtn: function(player) {
+    let btn = $('<button>').addClass('showdown-btn');
+    btn.attr('id', player+'-showdown').text('SHOWDOWN');
+    $('#'+player+'-btn-box').html(btn);
+    clicks.showDown(player);
   },
   activeChoice: function(player, choice) {
     switch (choice) {
@@ -220,6 +198,41 @@ var display = {
                    $('#'+player+'-sciss').addClass('active-rps');
                    break;
     }
+  },
+  arena: function() {
+    let timerDiv = $('<div>').addClass('timer');
+    let timerSpan = $('<span>').attr('id', 'timer');
+    timerDiv.html("Timer: ").append(timerSpan);
+
+    let resultDiv = $('<div>').addClass('result');
+    let resultSpan = $('<span>').attr('id', 'result');
+    resultDiv.html("Result: ").append(resultSpan);
+
+    $('.arena').html(timerDiv).append(resultDiv);
+  },
+  results: function(p1, p2, p1Choice, p2Choice) {
+    let p1Throw, p2Throw;
+    if (p1Choice === 0) {
+      p1Throw = "<p>" + p1 + "threw rock.</p>";
+    } else if (p1Choice === 1) {
+      p1Throw = "<p>" + p1 + " threw paper.</p>";
+    } else if (p1Choice === 2) {
+      p1Throw = "<p>" + p1 + " threw scissors.</p>";
+    } else {
+      p1Throw = "<p>" + p1 + " was too slow...</p>";
+    }
+
+    if (p2Choice === 0) {
+      p2Throw = "<p>" + p2+ "threw rock.</p>";
+    } else if (p2Choice === 1) {
+      p2Throw = "<p>" + p2 + " threw paper.</p>";
+    } else if (p2Choice === 2) {
+      p2Throw = "<p>" + p2 + " threw scissors.</p>";
+    } else {
+      p2Throw = "<p>" + p2 + " was too slow...</p>";
+    }
+
+    $('#result').html(p1Throw).append(p2Throw);
   }
 }
 
@@ -248,7 +261,7 @@ var timer = {
 
       if (timer <= 0) {
         clearInterval(intervalId);
-        display.showdownBtn();
+        display.showdownBtn(whoAmI);
       }
     }, 1000);
   }
@@ -256,6 +269,37 @@ var timer = {
 
 var db = {
   deleteInactive: function() {
+
+  },
+  updateHistory: function(num, p1Hist, p2Hist, p1, p2, p1Choice, p2Choice) {
+    display.results(p1, p2, p1Choice, p2Choice);
+
+    switch (num) {
+      case 0: $('#result').append("<p>You tied!</p>");
+              p1Hist[num]++; p2Hist[num]++; break;
+      case 1: $('#result').append("<p>" + p1 + " won!</p>");
+              p1Hist[num]++; p2Hist[num+1]++; break;
+      case 2: $('#result').append("<p>" + p2 + " won!</p>");
+              p1Hist[num]++; p2Hist[num-1]++; break;
+      default: break;
+    }
+    let storeData = {
+      p1RPS: -1,
+      p2RPS: -1,
+      p1Hist: p1Hist,
+      p2Hist: p2Hist,
+      p1Ready: false,
+      p2Ready: false,
+      timer: -1,
+      showdown: false
+    }
+    database.ref('/games/' + $('.arena').data('gameid') + '/')
+                              .update(storeData);
+  },
+  updateActive: function(gameId) {
+    database.ref('/games/' + gameId + '/').update({active: true});
+  },
+  updateSignOut: function() {
 
   }
 }
